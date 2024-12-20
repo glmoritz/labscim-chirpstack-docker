a6838e9967eae21d24f762f6dcaca1f78da6a1e0
Marcos Eduardo P. Monteiro <marcoseduardopm@hotmail.com>
2024-12-20 18:06:44 -0300

    Updated fix_permissions.sh

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+find ./postgresqldata/ -type f -name ".gitkeep" -delete
 chmod 777 -R ./contiki-ng
 chmod 777 -R ./labscim
 chmod 777 -R ./lora_gateway
 chmod 777 -R ./LoRaMac-node
 chmod 777 -R ./packet_forwarder
 chmod 777 -R ./sim
+chmod 777 -R ./postgresqldata
